8850ef7f52e5e2182533982e6884a06e0d554b96
hossein <hosseinbeigi26@gmail.com>
2024-11-18 20:01:23 +0330

    complete animation

--- app/src/main/java/com/github/meypod/al_azan/ui/alarm/AzanAlarm.kt
+++ app/src/main/java/com/github/meypod/al_azan/ui/alarm/AzanAlarm.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -37,14 +38,15 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageShader
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,6 +56,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.github.meypod.al_azan.R
 import com.github.meypod.al_azan.ui.theme.AlAzanTheme
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -66,27 +69,29 @@ fun AzanAlarm(
     val configuration = LocalConfiguration.current
     val screenHeightDp = configuration.screenHeightDp
     val density = LocalDensity.current.density
-    val initialOffsetY = remember(screenHeightDp) {
-        (screenHeightDp * 0.4f * density) // 90% of screen height in pixels
-    }
-
+    val initialOffsetY = remember(screenHeightDp) { (screenHeightDp * 0.4f * density) }
     val scope = rememberCoroutineScope()
-    //val swipeOffset = remember { Animatable(0f) }
-    val screenHeight = initialOffsetY
     val offsetY = remember { Animatable(initialOffsetY) }
     val isDragging = remember { mutableStateOf(false) }
-
     val cornerRadius = remember { Animatable(60f) }
     val size = remember { Animatable(244f) }
     var showDialog by remember { mutableStateOf(false) }
     var showButton by remember { mutableStateOf(true) }
     var showText by remember { mutableStateOf(true) }
+    val colors = listOf(
+        Color.White,
+        Color.White.copy(alpha = 0.5f),
+        Color.White.copy(alpha = 0.5f),
+        Color.White.copy(alpha = 0.5f)
+    ) // Define your color list
+    var currentColorIndex by remember { mutableIntStateOf(0) }
+
     AlAzanTheme {
         LaunchedEffect(showDialog) {
-            if (showDialog){
+            if (showDialog) {
                 showButton = false
                 showText = false
-            }else{
+            } else {
                 showButton = true
                 showText = true
             }
@@ -101,6 +106,12 @@ fun AzanAlarm(
                 }
             ) {
                 AlarmDialog()
+            }
+        }
+        LaunchedEffect(key1 = Unit) {
+            while (true) {
+                delay(150)
+                currentColorIndex = (currentColorIndex + 1) % colors.size
             }
         }
         Box(
@@ -126,6 +137,22 @@ fun AzanAlarm(
                     alpha = 0.04f
                 )
         ) {
+            if (isDragging.value) {
+                Column(
+                    modifier = modifier
+                        .align(Alignment.BottomCenter)
+                        .offset(y = (-160).dp)
+                ) {
+                    for (i in 1 until 4) {
+                        val iconColor = colors[(currentColorIndex + i - 1) % colors.size]
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.ic_arrow_up),
+                            contentDescription = "",
+                            tint = iconColor
+                        )
+                    }
+                }
+            }
             Column(
                 modifier
                     .padding(top = dimensionResource(R.dimen.last_card_padding_intro))
@@ -155,37 +182,32 @@ fun AzanAlarm(
                     style = MaterialTheme.typography.headlineLarge
                 )
             }
-
             if (showButton) {
-
                 Column(
                     modifier
-                        //.align(Alignment.CenterHorizontally)
-                        .fillMaxSize().padding(bottom = dimensionResource(R.dimen.padding_extra_extra_large))
-                    //.padding(bottom = dimensionResource(R.dimen.last_card_padding_intro))
-                    ,
+                        .fillMaxSize()
+                        .padding(bottom = dimensionResource(R.dimen.padding_extra_extra_large)),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    Text(
-                        text = "Hold and Swipe Up For More Option",
-                        color = Color.White,
-                        fontSize = 14.sp,
-                        style = MaterialTheme.typography.labelLarge,
-                        modifier = Modifier.offset {
-                            IntOffset(
-                                x = 0,
-                                y = initialOffsetY.roundToInt()
-                            )
-                        }
-                        //.align(Alignment.CenterHorizontally)
-                        // .padding(bottom = 12.dp)
-                    )
+                    if (!isDragging.value) {
+                        Text(
+                            text = "Hold and Swipe Up For More Option",
+                            color = Color.White,
+                            fontSize = 14.sp,
+                            style = MaterialTheme.typography.labelLarge,
+                            modifier = Modifier.offset {
+                                IntOffset(
+                                    x = 0,
+                                    y = initialOffsetY.roundToInt()
+                                )
+                            }
+                        )
+                    }
                     Spacer(modifier.padding(dimensionResource(R.dimen.spacer_items)))
                     Box(
                         contentAlignment = Alignment.Center,
                         modifier = Modifier
-                            //.align(Alignment.CenterHorizontally)
                             .size(width = size.value.dp, height = 100.dp)
                             .offset { IntOffset(x = 0, y = offsetY.value.roundToInt()) }
                             .background(
@@ -211,27 +233,21 @@ fun AzanAlarm(
                                                 animationSpec = tween(durationMillis = 300)
                                             )
                                         }
-
                                         isDragging.value = false
                                     },
                                     onDragEnd = {
                                         if (isDragging.value) {
-                                            //  Animate to the final position
-                                            if (offsetY.value < screenHeight * 0.7) {
+                                            if (offsetY.value < initialOffsetY * 0.7) {
                                                 scope.launch {
                                                     showDialog = true
-
-                                                    val targetOffset = offsetY.value / 2.2f
-                                                    // if (swipeOffset.value > 150f) 300f else 0f
                                                     offsetY.animateTo(
-                                                        targetValue = screenHeight,
+                                                        targetValue = initialOffsetY,
                                                         animationSpec = tween(durationMillis = 300)
                                                     )
                                                 }
-                                            }else{
+                                            } else {
                                                 scope.launch {
                                                     showDialog = false
-
                                                     offsetY.animateTo(
                                                         targetValue = initialOffsetY,
                                                         animationSpec = tween(300)
@@ -259,21 +275,17 @@ fun AzanAlarm(
                                         isDragging.value = true
                                         val newOffset = offsetY.value + dragAmount.y
                                         scope.launch {
-                                            //swipeOffset.snapTo(swipeOffset.value + dragAmount.y)
                                             offsetY.snapTo(
                                                 newOffset.coerceIn(
-                                                    screenHeight / 10,
-                                                    screenHeight
+                                                    initialOffsetY / 10,
+                                                    initialOffsetY
                                                 )
                                             )
                                         }
-//
                                     }
-
                                 )
                             })
                     {
-
                         Row {
                             Icon(
                                 painter = painterResource(R.drawable.clock),
@@ -293,31 +305,10 @@ fun AzanAlarm(
                                     modifier = Modifier
                                 )
                             }
-
                         }
-
-
                     }
                 }
             }
-
         }
     }
 }
-
-@Composable
-fun getScreenHeight(): Dp {
-    val configuration = LocalConfiguration.current
-    val screenHeightDp = configuration.screenHeightDp / 8
-    return screenHeightDp.dp
-}
-
-@Composable
-fun getBottomOffset(): Dp {
-    val configuration = LocalConfiguration.current
-    val screenHeightDp = configuration.screenLayout // Total height in dp
-    return screenHeightDp.dp // Convert to pixels
-}
-
-
-
